1c62cb4062306d24d50cec27e5c234a0c9294f8b
rigoalc <61516106+rigoalc@users.noreply.github.com>
2021-09-24 09:51:24 -0400

    Update Research Web Scraping.py

--- Research Web Scraping.py
+++ Research Web Scraping.py
@@ -3,16 +3,10 @@
 # CIS-216-12292 
 # Python Programming
 
-#Header with your name, date, CRN, and class name-
-#2-4 paragraphs explaining the following:
-#What is web scraping?
-#What are two or more reasons why websites are scraped?
-#Why would a website owner not want their website scraped?
-#Is web scraping legal in the US? Other countries? (This is not a yes/no, just explain what you find)
-#1 paragraph on your opinion: Do you think scraping websites is ethical? Why or why not?
-#All paragraphs are 3-5 sentences
+
 
 print('''
+What is web scraping?
 Web scraping is the process of using bots to extract content and data from a website. The scraper can then replicate the entire website content elsewhere. Web scraping is used in a variety of digital businesses that rely on data harvesting. 
 Web scraping is a technique for extracting information from websites. This can be done manually but it is usually faster, more efficient, and less error-prone to automate the task.
 Web scraping allows you to acquire non-tabular or poorly structured data from websites and convert it into a usable, structured format, such as a .csv file or spreadsheet.
@@ -20,21 +14,24 @@ Scraping is about more than just acquiring data: it can also help you archive da
 It is closely related to the practice of web indexing, which is what search engines like Google do when mass-analyzing the Web to build their indices. But contrary to web indexing,
 which typically parses the entire content of a web page to make it searchable, web scraping targets specific information on the pages visited.
 
+What are two or more reasons why websites are scraped?
 Web scraping can help to get potential customers, analyze people's interests and monitor consumer sentiment by regularly extracting customer ratings from different platforms. 
 In E-Commerce, Web Scraping is used for competition monitoring and price comparisons.
 In Marketing, Web Scraping is used for lead generation, to build phone and email lists for cold outreach.
 In Real Estate, Web Scraping is used to collect property details as well as contact details of agents and owners.
 Web Scraping is used to collect training data for Machine Learning models.
 
+Why would a website owner not want their website scraped?
 Site scraping can be a powerful tool. In the right hands, it automates the gathering and dissemination of information. 
 In the wrong hands, it can lead to theft of intellectual property or an unfair competitive edge.
 
+Is web scraping legal in the US? Other countries? (This is not a yes/no, just explain what you find)
 Web scraping It is perfectly legal in the United States. If you scrape data from websites for public consumption and use it for analysis. However, it is illegal if you scrape confidential information for profit.
 For example: scraping private contact information without permission and sell them to a 3rd party for profit is illegal.
-
 Under the EU's General Data Protection Regulation (GDPR), web scraping restrictions do not apply to a person or company unless such an entity extracts personal data of people within the European Economic Area.
 In Canada, the legality of web scraping has not been fully defined. In 2011, a B.C. court sided with a company that accused another website of scraping its content without authorization.
 
+1 paragraph on your opinion: Do you think scraping websites is ethical? Why or why not?
 Data scraping is ethical as long as the scraping bot respects all the rules, the copyrights, the people's privacy set by the websites, and that the scraped data is used with good intentions. 
-
+I think that we can say that the method is not the illegal part, in this case, the objective will determine if the action is legal or illegal. Scraping is a method but the objective of the action is the situation to judge.
 ''')
